cbe3ca4b8e0dc2e473b5ae55f3d07ccc85cd60a1
Maryam-tli <m.tavakoolii1400@gmail.com>
2024-11-25 19:20:39 +0330

    Set up database connection and session management

--- PostgreSQL_Connection.py
+++ PostgreSQL_Connection.py
@@ -1,2 +1,11 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+
+# Define the connection string
+connection_string = "postgresql+psycopg2://your_username:your_password@localhost:5432/your_database"
+
+# Create the database engine, a base class, Set up a session factory and Create a session instance
+motor = create_engine(connection_string)
+Base = declarative_base()
+meeting = sessionmaker(bind=motor)
+session = meeting()
